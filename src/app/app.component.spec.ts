79b6c580a02caeb4d61632289680cc14104b5e6b
Angel González <agonzalezbarragan@gmail.com>
2021-08-06 12:57:39 +0200

    CHANGED: * limpieza

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed } from '@angular/core/testing';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CartModule } from './views/cart/cart.module';
